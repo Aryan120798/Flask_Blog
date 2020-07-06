89d3b7f3a5373fe4dded9ca61b06b8f87b0c4036
Aryan Deore <aryandeore98@gmail.com>
2020-07-06 20:38:23 +0530

    added custom validation to check if username and email already exists

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 import email_validator
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -11,6 +12,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    # custom validation to check if username and email alreeady exists
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken. Please choose another one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already Exists. Please choose another one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
